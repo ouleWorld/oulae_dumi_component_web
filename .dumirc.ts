8458cdf3bc26a1301ee4cad0387b01b232268479
oulae <hcylddzz@163.com>
2023-03-26 17:22:03 +0800

    feat: 修改路由配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  base: '/',
-  publicPath: '/',
+  base: '/oulae_dumi_component_web',
+  publicPath: '/oulae_dumi_component_web',
   themeConfig: {
     name: 'oulae_dumi_component_web',
   },
