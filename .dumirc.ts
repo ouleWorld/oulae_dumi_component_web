b50ff38bee9d68582856d49068dc78deec930e2c
oulae <hcylddzz@163.com>
2023-03-28 22:44:02 +0800

    feat: 修改 dumi 配置信息

--- .dumirc.ts
+++ .dumirc.ts
@@ -5,6 +5,7 @@ const repo = 'oulae_dumi_component_web';
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  outputPath: 'docs-dist',
   themeConfig: {
     name: 'oulae_dumi_component_web',
   },
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,9 @@
 module.exports = {
+  pluginSearchDirs: false,
+  plugins: [
+    require.resolve('prettier-plugin-organize-imports'),
+    require.resolve('prettier-plugin-packagejson'),
+  ],
   printWidth: 80,
   proseWrap: 'never',
   singleQuote: true,
