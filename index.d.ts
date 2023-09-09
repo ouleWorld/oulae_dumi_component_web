0c2ced48b2c9a1e02876bd293a19382098f1cc96
ouleWorld <ouleWorld@users.noreply.github.com>
2023-09-09 18:57:03 +0000

    deploy: f57cafe19422063c931b621934b16a2c2570745d

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-import PopoverDialog from './PopoverDialog';
-import ReactWindowStickyCells from './ReactWindowStickyCells';
-import RelationComponent from './Relation';
-export * from './Relation';
-export { RelationComponent, PopoverDialog, ReactWindowStickyCells };
